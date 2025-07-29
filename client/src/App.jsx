d16d008fb386f6f676d01a3be270c4710986bb09
Nazmul Limon <139962263+limon-available@users.noreply.github.com>
2025-07-29 10:47:02 +0600

    case insensitive

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 
 import {Outlet} from 'react-router'
 import './App.css'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import { BookProvider } from './context/BookContext'
 function App() {
   
