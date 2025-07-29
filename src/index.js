6b5642ea5943f697f61c1169370e417066433db9
Nazmul Limon <139962263+limon-available@users.noreply.github.com>
2025-07-29 11:19:26 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
- const express = require('express');
+require('dotenv').config();
+const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const port = 3000;
